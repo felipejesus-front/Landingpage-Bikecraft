cdfa185a0120da6267fc9e7e19b95c6d2183f7e1
Felipe Campos <felipe.campos@al.resilia.com.br>
2022-08-17 13:55:34 -0300

    adicionando abrir pagina de compra com produto selecionado anteriormente

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+// ativar links do menu  
 const links = document.querySelectorAll(".header-menu a");
-
 
 function ativarLink(link){
     const url = location.href
     const href = link.href
-
     
     if (url.includes(href)) {
         link.classList.add('ativo')
     }
 }
+links.forEach(ativarLink);
 
-links.forEach(ativarLink)
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true;
+    }
+}
+parametros.forEach(ativarProduto);
